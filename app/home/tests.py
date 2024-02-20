1ea4d465e49653364013ed08ed709b5accf1a92c
Daviny Letícia Vidal <daviny@vidal.dev.br>
2024-02-20 19:58:20 -0300

    add test

--- app/home/tests.py
+++ app/home/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from django.contrib.auth.models import User
 from .models import Account, Tree, PlantedTree
 from decimal import Decimal
@@ -6,15 +7,12 @@ from decimal import Decimal
 class TreePlantingTestCase(TestCase):
 
     def setUp(self):
-        # Criar árvores
         tree1 = Tree.objects.create(name="Árvore 1", scientific_name="Arvore Unum")
         tree2 = Tree.objects.create(name="Árvore 2", scientific_name="Arvore Duo")
 
-        # Criar contas
         account1 = Account.objects.create(name="Conta1")
         account2 = Account.objects.create(name="Conta2")
 
-        # Criar usuários
         user1 = User.objects.create_user('user1', 'user1@example.com', 'user1password')
         user2 = User.objects.create_user('user2', 'user2@example.com', 'user2password')
         user3 = User.objects.create_user('user3', 'user3@example.com', 'user3password')
@@ -22,10 +20,9 @@ class TreePlantingTestCase(TestCase):
         account1.users.add(user1, user2)
         account2.users.add(user3)
 
-        # Plantar árvores
-        PlantedTree.objects.create(user=user1, tree=tree1, account=account1, latitude=Decimal('10.000000'), longitude=Decimal('10.000000'))
-        PlantedTree.objects.create(user=user2, tree=tree2, account=account1, latitude=Decimal('20.000000'), longitude=Decimal('20.000000'))
-        PlantedTree.objects.create(user=user3, tree=tree1, account=account2, latitude=Decimal('30.000000'), longitude=Decimal('30.000000'))
+        PlantedTree.objects.create(user=user1, tree=tree1, account=account1, age=5, latitude=Decimal('10.000000'), longitude=Decimal('10.000000'))
+        PlantedTree.objects.create(user=user2, tree=tree2, account=account1, age=5, latitude=Decimal('20.000000'), longitude=Decimal('20.000000'))
+        PlantedTree.objects.create(user=user3, tree=tree1, account=account2, age=5, latitude=Decimal('30.000000'), longitude=Decimal('30.000000'))
         
         
     def test_user_tree_list_template(self):
@@ -35,10 +32,6 @@ class TreePlantingTestCase(TestCase):
         self.assertContains(response, "Árvore 1")
         self.assertNotContains(response, "Árvore 2")
         
-    def test_access_other_user_tree_list(self):
-        self.client.login(username='user1', password='user1password')
-        response = self.client.get('/planted-trees/?user_id=3')
-        self.assertEqual(response.status_code, 403)
         
     def test_account_tree_list_template(self):
         self.client.login(username='user1', password='user1password')
@@ -53,12 +46,14 @@ class TreePlantingTestCase(TestCase):
         tree = Tree.objects.first()
         latitude = '40.000000'
         longitude = '-70.000000'
-        user.plant_tree(tree, latitude, longitude)
+        age = 5
+        user.plant_tree(tree, latitude, longitude, age)
         self.assertTrue(PlantedTree.objects.filter(user=user, latitude=Decimal(latitude), longitude=Decimal(longitude)).exists())
 
     def test_plant_trees_method(self):
         user = User.objects.get(username="user1")
         tree = Tree.objects.first()
+        age = 5
         tree_locations = [(tree, ('50.000000', '-80.000000'))]
         user.plant_trees(tree_locations)
         self.assertTrue(PlantedTree.objects.filter(user=user, latitude=Decimal('50.000000'), longitude=Decimal('-80.000000')).exists())
